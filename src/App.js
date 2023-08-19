5307505e7ee2fded87580beebbf3842cbc7b8532
minhthuan <minhthuan26042001@gmail.com>
2023-08-19 16:33:56 +0700

    init router

--- src/App.js
+++ src/App.js
@@ -2,9 +2,35 @@ import './App.css';
 
 function App() {
 	return (
-		<div className="App">
+		<BrowserRouter>
+			<Routes>
+				{
+					publicRoutes.map((route, index) => {
+						const Page = route.page
+						return (
+							<Route
+								key={index}
+								path={route.path}
+								element={<Page />} />
+						)
+					})}
 
-		</div>
+				{
+					privateRoutes.map((route, index) => {
+						const Page = route.page
+						return (
+							<Route key={index} element={<SecureRoute />}>
+								<Route
+									key={index}
+									path={route.path}
+									element={<Page />} />
+							</Route>
+						)
+					})
+				}
+
+			</Routes>
+		</BrowserRouter>
 	);
 }
 
